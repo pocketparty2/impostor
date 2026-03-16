22a451d17f831d7d868b022ca0f800089cbe1e45
pocketparty2 <ilikefindtheshampoos@gmail.com>
2026-03-16 21:48:57 +0000

    Create words.js

---
+++ words.js
@@ -0,0 +1,17 @@
+// words.js
+const WORDS = {
+  animals: [
+    { word: "Elephant", hint: "Large grey animal" },
+    { word: "Tiger", hint: "Striped predator" },
+    { word: "Kangaroo", hint: "Jumps and has a pouch" }
+  ],
+  food: [
+    { word: "Pizza", hint: "Italian dish" },
+    { word: "Sushi", hint: "Japanese rice rolls" },
+    { word: "Burger", hint: "Fast food sandwich" }
+  ],
+  objects: [
+    { word: "Laptop", hint: "Portable computer" },
+    { word: "Umbrella", hint: "Used in the rain" }
+  ]
+};
